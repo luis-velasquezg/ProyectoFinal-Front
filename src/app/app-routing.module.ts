e2161c9261520411ef0f2b1e47ae433ea6a24f67
Luis Velasquez <luis.velasquezg@udea.edu.co>
2020-04-23 00:42:45 -0500

    Creacion de los componentes: Inventario, Ventas, Compras, Facturacion, Recursos Humanos, Login, Home. Y se configuraron las rutas del sitio web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ComprasComponent } from './pages/compras/compras.component';
+import { FacturacionComponent } from './pages/facturacion/facturacion.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RecursosHumanosComponent } from './pages/recursos-humanos/recursos-humanos.component';
+import { VentasComponent } from './pages/ventas/ventas.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'compras', component: ComprasComponent },
+  // { path: 'facturacion', component: FacturacionComponent },
+  { path: 'inventario', component: InventarioComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'recursos-humanos', component: RecursosHumanosComponent },
+  { path: 'ventas', component: VentasComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: false } )
+  ],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
+import { VentasComponent } from './pages/ventas/ventas.component';
+import { ComprasComponent } from './pages/compras/compras.component';
+import { FacturacionComponent } from './pages/facturacion/facturacion.component';
+import { RecursosHumanosComponent } from './pages/recursos-humanos/recursos-humanos.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InventarioComponent,
+    VentasComponent,
+    ComprasComponent,
+    FacturacionComponent,
+    RecursosHumanosComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
